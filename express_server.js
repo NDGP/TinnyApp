cd1f910be12ea50993c4ee1b1913ffdeece2ce16
Nathan <nathanpatrick939@gmail.com>
2020-11-09 23:45:03 +0000

    get / post work

--- express_server.js
+++ express_server.js
@@ -1,3 +1,6 @@
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -32,6 +35,10 @@ app.get("/set", (req, res) => {
  app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
