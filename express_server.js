65bca524514c22791bd45971c0f6ae2d4e488b5c
Nathan <nathanpatrick939@gmail.com>
2020-11-11 22:44:42 +0000

    registration working

--- express_server.js
+++ express_server.js
@@ -58,26 +58,34 @@ app.get("/set", (req, res) => {
  });
  
  app.get("/urls", (req, res) => {
-  console.log(req.cookies)
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  //console.log(req.cookies)
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
   res.render("urls_index", templateVars);
+  //console.log()
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
+//working here//////////
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.url,  username: req.cookies["username"] };
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: req.params.url,  
+    user: users[req.cookies["user_id"]]
+  };
+
   res.render("urls_show", templateVars);
+  console.log(templateVars.user)
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body.longURL); 
+  //console.log(req.body.longURL); 
   let newURL = generateRandomString();
   urlDatabase[newURL] = req.body.longURL;
-  console.log(urlDatabase.shortURL)
   res.redirect(`/urls/${newURL}`);
 });
 
@@ -89,10 +97,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL]
-  res.redirect("/urls")
-});
+// add and deleat urls from list
 
 app.post("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
@@ -100,10 +105,15 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls")
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+});
+
+// login and logout
+
 app.post("/login", (req, res) => {
-  console.log(req.body)
-  let newUser = req.body.username
-  res.cookie("username", newUser)
+  
   res.redirect("/urls")
 });
 
@@ -112,11 +122,42 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls")
 });
 
+// registration section
+
 app.get("/register", (req, res) => {
-  console.log(req.body)
-  res.render("register")
+  const templateVars =  {error: undefined}
+  res.render("register", templateVars)
 });
 
 app.post("/register", (req, res) => {
-  console.log(req.body)
+  //console.log(req.body)
+  for (let user in users){
+   // console.log("user log", users[user])
+  if(users[user].email === req.body.email){
+    const templateVars =  {error: "error coad 400 : email already exists"}
+    return res.render("register", templateVars)
+  }
+  if(req.body.email === "" || req.body.password === ""){
+    const templateVars =  {error: "error coad 400 : all fields must be filled"}
+    return res.render("register", templateVars)
+  }
+}
+  let randomID = generateRandomString()
+  req.body.id = randomID
+  users[randomID] = req.body
+  res.cookie("user_id", randomID )
+  res.redirect("/urls")
 });
+
+// const users = { 
+//   "userRandomID": {
+//     id: "userRandomID", 
+//     email: "user@example.com", 
+//     password: "purple-monkey-dinosaur"
+//   },
+//  "user2RandomID": {
+//     id: "user2RandomID", 
+//     email: "user2@example.com", 
+//     password: "dishwasher-funk"
+//   }
+// }
