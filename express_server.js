358344f38f7d5a21ce25b06e6fac940bca9e7c31
Nathan <nathanpatrick939@gmail.com>
2020-11-11 01:24:29 +0000

    logout

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
+const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 app.set("view engine", "ejs") 
 
 // new url maker
@@ -40,16 +42,19 @@ app.get("/set", (req, res) => {
  });
  
  app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+   //console.log(Object.keys(req))
+   console.log(req.cookies)
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.url };
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.url,  username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -76,3 +81,21 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls")
 });
+
+app.post("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls")
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body)
+  let newUser = req.body.username
+  res.cookie("username", newUser)
+  res.redirect("/urls")
+});
+
+app.post("/logout", (req, res) =>{
+  res.clearCookie('username', req.body.username)
+  res.redirect("/urls")
+})
