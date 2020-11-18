f67e89aca1b93ae3c9569cfcbabad906a06cd017
Nathan <nathanpatrick939@gmail.com>
2020-11-18 01:22:23 +0000

    up to read me part

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
-const cookieSession = require('cookie-session')
-const {getUserByEmail, generateRandomString, urlsForUser} = require('./helpers')
+const cookieSession = require('cookie-session');
+const {getUserByEmail, generateRandomString, urlsForUser} = require('./helpers');
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
@@ -10,9 +10,9 @@ app.use(cookieSession({
   name: 'session',
   keys: ["key1", "key2"],
   maxAge: 24 * 60 * 60 * 1000
-  })
+})
 );
-app.set("view engine", "ejs") 
+app.set("view engine", "ejs");
 
 //user object 
 const users = { 
@@ -21,12 +21,12 @@ const users = {
     email: "user@example.com", 
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
+  "user2RandomID": {
     id: "user2RandomID", 
     email: "user2@example.com", 
     password: "dishwasher-funk"
   }
-}
+};
 
 // new url maker
 
@@ -59,157 +59,140 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
+});
 
- //URL url colection and new url
- 
- app.get("/urls", (req, res) => {
-  //console.log(req.cookies)
-  const userID = req.session.user_id 
-  if (!userID){
+// rendering the url page with appropriet data showing 
+
+app.get("/urls", (req, res) => {
+  const userID = req.session.user_id;
+  if (!userID) {
     const templateVars = { user: ""};
     res.render("urls_index", templateVars);
-  }else{
-  const userURLdatabase = urlsForUser(userID, urlDatabase)
-  const templateVars = { urls: userURLdatabase, user: users[userID] };
-  res.render("urls_index", templateVars);
+  } else {
+    const userURLdatabase = urlsForUser(userID, urlDatabase);
+    const templateVars = { urls: userURLdatabase, user: users[userID] };
+    res.render("urls_index", templateVars);
   }
 });
 
+// rendering creat new page and blocking if user is not loged in or registerd 
 
 app.get("/urls/new", (req, res) => {
-  const userID = req.session.user_id 
+  const userID = req.session.user_id;
   const templateVars = { user: users[userID] };
-  if(!userID){
-    res.redirect("/login")
-  }else{
-  res.render("urls_new", templateVars);
+  if (!userID) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_new", templateVars);
   }
 });
+
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const userID = req.session.user_id 
-    const templateVars = { 
+  const userID = req.session.user_id;
+  const templateVars = { 
     shortURL: req.params.shortURL, 
     longURL: req.params.url,  
     user: users[userID]
   };
 
-  if(!userID){
-    return res.redirect("/urls", templateVars)
-  }else{
-  res.render("urls_show", templateVars);
-  //console.log(templateVars.user)
+  if (!userID) {
+    return res.redirect("/urls", templateVars);
+  } else {
+    res.render("urls_show", templateVars);
+  
   }
 });
 
+// generating new short url for users input of long url
 
 app.post("/urls", (req, res) => {
-  console.log(req.body.longURL); 
   let newURL = generateRandomString();
-  urlDatabase[newURL]= {longURL : req.body.longURL, userID : req.session.user_id};
+  urlDatabase[newURL] = {longURL : req.body.longURL, userID : req.session.user_id};
   res.redirect(`/urls`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  if (urlDatabase[req.params.shortURL] === undefined){
-    return res.redirect("https://www.youtube.com/watch_popup?v=N9wsjroVlu8&t=16s")
+  if (urlDatabase[req.params.shortURL] === undefined) {
+    return res.redirect("https://www.youtube.com/watch_popup?v=N9wsjroVlu8&t=16s");
   }
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-// add and deleat urls from list
+//  edit and deleat urls from list
 
 app.post("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   const userID = req.session.user_id;
-  if(!userID){
-    res.redirect("/urls")
-  }else{
-  urlDatabase[shortURL] = {longURL : req.body.longURL, userID : req.session.user_id};
-  res.redirect("/urls");
+  if (!userID) {
+    res.redirect("/urls");
+  } else {
+    urlDatabase[shortURL] = {longURL : req.body.longURL, userID : req.session.user_id};
+    res.redirect("/urls");
   }
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-    const userID = req.session.user_id;
-    if(!userID) {
-      res.redirect("/urls");
-    }else {
-      delete urlDatabase[req.params.shortURL]
-      res.redirect("/urls");
+  const userID = req.session.user_id;
+  if (!userID) {
+    res.redirect("/urls");
+  } else {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
   }
 });
 
-// login and logout
+// login and logout, renders login page, acceptets user data from login testing if user existes in database
 
 app.get("/login", (req, res) => {
-  const templateVars =  {error: undefined}
-  res.render("login", templateVars)
-})
+  const templateVars =  {error: undefined};
+  res.render("login", templateVars);
+});
 
 
 app.post("/login", (req, res) => {
-  const existingUser = getUserByEmail(req.body.email, users)
-  if (!existingUser || !bcrypt.compareSync(req.body.password, users[user].password)){
-  }else if(existingUser && bcrypt.compareSync(req.body.password, users[user].password)){
-    req.session.user_id = users[user].id
-    return res.redirect("/urls")
+  const existingUser = getUserByEmail(req.body.email, users);
+  if (existingUser && bcrypt.compareSync(req.body.password, users[existingUser].password)) {
+    req.session.user_id = users[existingUser].id;
+    return res.redirect("/urls");
   }
 
-  const templateVars =  {error: "403 error : hmmmm try again"}
-  res.render("login", templateVars)
+  const templateVars =  {error: "403 error : hmmmm try again"};
+  res.render("login", templateVars);
 });
 
 app.post("/logout", (req, res) => {
-  req.session = null
-  res.redirect("/urls")
+  req.session = null;
+  res.redirect("/urls");
 });
 
-// registration section
+// registration section and fileing of registerd user 
 
 app.get("/register", (req, res) => {
-  const templateVars =  {error: undefined}
-  res.render("register", templateVars)
+  const templateVars =  {error: undefined};
+  res.render("register", templateVars);
 });
 
 app.post("/register", (req, res) => {
-  //console.log(req.body)
-  const existingUser = getUserByEmail(req.body.email, users)
-  if (existingUser){
-    const templateVars =  {error: "error coad 400 : email already exists"}
-    return res.render("register", templateVars)
+  const existingUser = getUserByEmail(req.body.email, users);
+  if (existingUser) {
+    const templateVars =  {error: "error coad 400 : email already exists"};
+    return res.render("register", templateVars);
   }
-    if(req.body.email === "" || req.body.password === ""){
-      const templateVars =  {error: "error coad 400 : all fields must be filled"}
-      return res.render("register", templateVars)
-    }
-  const pass = req.body.password
+  if (req.body.email === "" || req.body.password === "") {
+    const templateVars =  {error: "error coad 400 : all fields must be filled"};
+    return res.render("register", templateVars);
+  }
+  const pass = req.body.password;
   const hashedPassword = bcrypt.hashSync(pass, 10);
-
-  console.log(existingUser)
-  let randomID = generateRandomString()
-  req.body.id = randomID
-  users[randomID] = { email: req.body.email, password: hashedPassword }
-  console.log(users)
-  req.session.user_id = randomID 
-  res.redirect("/urls")
+  let randomID = generateRandomString();
+  req.body.id = randomID;
+  users[randomID] = { id: randomID, email: req.body.email, password: hashedPassword };
+  req.session.user_id = randomID; 
+  res.redirect("/urls");
 });
 
 
-module.exports = {urlDatabase}
-
-// const users = { 
-//   "userRandomID": {
-//     id: "userRandomID", 
-//     email: "user@example.com", 
-//     password: "purple-monkey-dinosaur"
-//   },
-//  "user2RandomID": {
-//     id: "user2RandomID", 
-//     email: "user2@example.com", 
-//     password: "dishwasher-funk"
-//   }
-// }
+module.exports = {urlDatabase};
